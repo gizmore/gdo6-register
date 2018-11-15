dda66d9d895d9de88548e8b61866306d29688b7c
gizmore <gizmore@wechall.net>
2018-11-15 03:33:45 +0100

    6.08-r77-de

---
+++ lang/register_de.php
@@ -0,0 +1,39 @@
+<?php
+return array(
+'btn_register' => 'Registrieren',
+'btn_guest' => 'Weiter als Gast',
+'ft_register_form' => '[%s] Registrierung',
+'tos_label' => 'Ich habe die <a href="%s">Nutzungsbedingungen</a> und <a href="%s">Datenschutzbestimmungen</a> gelesen.',
+'msg_activation_mail_sent' => 'Wir haben Dir eine E-Mail gesendet. Folge dem Aktivierungslink dort um die registrierung abzuschliessen.',
+'msg_activated' => 'Willkommen, %s, Dein Konto ist nun aktiviert.',
+'err_username_taken' => 'Dieser Benutzername ist bereits vergeben.',
+'err_email_taken' => 'Diese E-Mail ist bereits vergeben.',
+'err_register' => 'Bei der Registrierung sind noch Fehler aufgetreten.',
+'err_no_activation' => 'Die aktivierung ist fehlgeschlagen.',
+'err_ip_signup_max_reached' => 'Es wurden bereits %s Konten mit Ihrer IP erstellt. Bitte wartet etwas.',
+'err_tos_not_checked' => 'Sie müssen den Nutzungsbedingungen zustimmen um fortzufahren.',
+#############################################################################
+'ft_register_guest' => '[%s] Als Gast fortfahren.',
+'btn_signup_guest' => 'Nickname verwenden',
+'err_guest_name_taken' => 'Dieser Nickname ist bereits in Benutzung',
+'msg_registered_as_guest' => 'Du bist nun als %s angemeldet.',
+'msg_registered_as_guest_back' => 'Du bist nun als %s angemeldet. <a href="%s">Klicke hier</a> um fortzufahren.',
+'link_register' => 'zur Registrierung',
+'link_register_guest' => 'weiter als Gast',
+	
+#############################################################################
+'mail_activate_title'=> '%s: Activate your account',
+'mail_activate_body' =>
+"Hello %s\n".
+"\n".
+"Welcome to %s\n".
+"\n".
+"To activate your account please visit the following link.\n".
+"\n".
+"%s\n".
+"\n".
+"In case you do not want to register an account, please ignore this email.\n".
+"\n".
+"Kind Regards\n".
+'The %2$s staff',
+);
