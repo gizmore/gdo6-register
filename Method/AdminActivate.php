c8e2d3a5cac094d37e4abba20ad077dd10c48ba7
gizmore <gizmore@wechall.net>
2019-07-31 22:31:22 +0200

    6.10-r6_bugfix

--- Method/AdminActivate.php
+++ Method/AdminActivate.php
@@ -4,6 +4,7 @@ namespace GDO\Register\Method;
 use GDO\Core\Method;
 use GDO\Register\GDO_UserActivation;
 use GDO\Util\Common;
+use GDO\User\GDO_User;
 
 final class AdminActivate extends Method
 {
@@ -11,7 +12,13 @@ final class AdminActivate extends Method
 	public function execute()
 	{
 		$activation = GDO_UserActivation::table()->find(Common::getGetString('id'));
+		
+		# Activate wrapped in user change
+		$me  = GDO_User::current();
+		GDO_User::$CURRENT = GDO_User::ghost();
 		$user = Activate::make()->activateToken($activation);
+		GDO_User::$CURRENT = $me;
+		
 		return $this->message('msg_user_activated', [$user->displayNameLabel()]);
 	}
 }
